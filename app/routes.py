f948de17ea66dc66ebcc8fda6f111bdefe5a233f
Tx0sh1 <clydemojura@protonmail.com>
2025-07-30 20:46:03 +0200

    feat: style login form with Bootstrap + Flask-WTF, add navbar and flash message layout

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect
 
 @app.route('/')
 @app.route('/index')
@@ -13,3 +15,12 @@ def tickets():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
