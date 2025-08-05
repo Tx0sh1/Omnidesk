158d303fb4e10d019a58cda4d8581211787efac1
Tx0sh1 <clydemojura@protonmail.com>
2025-08-05 22:32:22 +0200

    Day 6: Implement user registration

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from flask import render_template
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
 from flask_login import logout_user
 from flask_login import login_required
+from app.forms import RegistrationForm
 
 
 @app.route('/')
@@ -38,6 +39,20 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        sa.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
 @app.route('/logout')
 def logout():
